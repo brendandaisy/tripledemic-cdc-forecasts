c030084085bdf5ba42a5bc244c5ae8c38761807b
brendandaisy <bcase@uvm.edu>
2023-10-23 12:29:09 -0400

    besag spatiotemporal model

--- flu-forecasts.R
+++ flu-forecasts.R
@@ -1,7 +1,12 @@
 library(tidyverse)
 library(INLA)
+library(sf)
+library(spdep)
+library(lubridate)
 
-# Read in current data to long format (Maya's pipeline here)----------------------
+source("model-fitting.R")
+
+# Read in current data to long format---------------------------------------------
 dat0 <- read_csv("data/counts_state_alldisease.csv") |> 
     rename(t=`...1`) |> 
     mutate(date=as.Date("2020-01-05") + lubridate::duration(t-1, "week"), .after=t) # starting from 1st Sunday of 2020 (epiweek 1)
@@ -11,13 +16,45 @@ dat <- dat0 |>
     separate_wider_delim(name, "_", names=c("state", "type")) |> 
     mutate(epiweek=as.numeric(str_sub_all(orig_index, 5, 6)), count=round(count))
 
-flu <- filter(dat, type == "flu", date > as.Date("2021-06-01"))
-flusight_quantiles <- TODO
+flu0 <- fetch_flu()
+
+# TODO: setting up `snum` and `us_adj` when there are a variable (< all 52) states 
+
+flu <- flu0 |> 
+    rename(state=location_name, count=value) |> 
+    group_by(date) |> 
+    mutate(t=cur_group_id(), epiweek=epiweek(date), .after=date) |> # add a time counter starting from 1 for earliest week
+    ungroup() |> 
+    filter(state != "US") |> # make sure US is not in training data
+    mutate(snum=as.numeric(fct_inorder(state))) # INLA needs groups as ints starting from 1, so add numeric state code
+
+us0 <- read_sf("data/us-state-boundaries.shp")
+
+us <- us0 |> 
+    filter(name %in% unique(flu$state)) |> 
+    select(state=name, division, region)
+
+state_order <- fct_inorder(unique(flu$state))
+
+# sort order of states to match their order of appearance in flu data
+us <- us |> 
+    mutate(state=fct_relevel(state, levels(state_order))) |> 
+    arrange(state)
+
+us_adj <- us |> 
+    poly2nb() |> 
+    nb2mat(style="B", zero.policy=TRUE)
+
+# flu <- filter(dat, type == "flu", date > as.Date("2021-06-01"))
+# flusight_quantiles <- TODO
 
 ###
 flu_pred <- prep_model_data(flu)
-fit <- fit_current_model(flu_pred)
+fit <- fit_current_model(flu_pred, graph=us_adj)
 summary(fit)
 
 summarize_predictions(flu_pred, fit)
-plot_predictions(flu_pred, fit, tspan=interval("2022-10-01", "2023-02-01"), file="figs/flu-predictions-10-11.pdf")
+plot_seasonal(flu_pred, fit)
+
+plot_predictions(flu_pred, fit, tback=5)
+plot_predictions(flu_pred, fit, tspan=interval("2022-10-01", "2023-02-01"))
--- model-fitting.R
+++ model-fitting.R
@@ -1,28 +1,43 @@
 library(tidyverse)
 library(INLA)
+library(lubridate)
 
-prep_model_data <- function(dat_long) {
-    # INLA needs groups as ints starting from 1, so add numeric state code
-    dat_long <- mutate(dat_long, snum=as.numeric(factor(state)))
-    
+prep_model_data <- function(dat_long, tspan=NULL) {
     # make a dataframe to hold group info for forecasting
-    pred_df <- expand_grid(
-        tibble(t=1:5+max(dat_long$t), epiweek=(0:4+last(dat_long$epiweek)) %% 53 + 1),
-        distinct(dat_long, state, snum)
-    )
+    if (is.null(tspan)) {
+        pred_df <- expand_grid(
+            tibble(
+                date=duration(1:5, "week") + max(dat_long$date),
+                t=1:5 + max(dat_long$t),
+                epiweek=epiweek(date)
+            ),
+            distinct(dat_long, state, snum)
+        )
+    } else {
+        dat_long <- filter(dat_long, tspan)
+    }
+    
     
     bind_rows(dat_long, pred_df) # add to data for counts to be NAs
 }
 
-fit_current_model <- function(df, q=c(0.025, 0.25, 0.5, 0.75, 0.975)) {
+fit_current_model <- function(df, q=c(0.025, 0.25, 0.5, 0.75, 0.975), graph=NULL) {
     # the PC priors c(u, a) give the probability that the standard deviation between weeks u is greater than a
     # increasing u increases prior beliefs that there will be large jumps in data between weeks; see `inla.doc("pc.prec")`
-    hyper_epwk <- list(theta=list(prior="pc.prec", param=c(0.5, 0.01))) # a priori the seasonal effect should be more smooth
-    hyper_wk <- list(theta=list(prior="pc.prec", param=c(1, 0.01))) # more favorable to large jumps
+    hyper_epwk <- list(prec=list(prior="pc.prec", param=c(0.5, 0.01))) # a priori the seasonal effect should be more smooth
+    hyper_wk <- list(prec=list(prior="pc.prec", param=c(1, 0.01))) # more favorable to large jumps
     
-    mod <- count ~ 1 + # intercept
-        f(epiweek, model="rw2", cyclic=TRUE, hyper=hyper_epwk) + # seasonal effect (currently shared over states)
-        f(t, model="ar1", group=snum, hyper=hyper_wk, control.group=list(model="exchangeable")) # weekly x state effect
+    if (is.null(graph)) {
+        sp_control <- list(model="exchangeable")
+        mod <- count ~ 1 + # intercept
+            f(epiweek, model="rw2", cyclic=TRUE, hyper=hyper_epwk) + # seasonal effect (currently shared over states)
+            f(t, model="ar1", group=snum, hyper=hyper_wk, control.group=sp_control) # weekly x state effect
+    } else {
+        sp_control <- list(model="besag", graph=graph, scale.model=TRUE)
+        mod <- count ~ 1 +
+            f(epiweek, model="rw2", cyclic=TRUE, hyper=hyper_epwk) +
+            f(snum, model="besagproper", graph=graph, group=t, hyper=hyper_wk, control.group=list(model="ar1"))
+    }
     
     fit <- inla(
         mod, family="poisson", data=df, # poisson regression link
@@ -56,23 +71,23 @@ plot_predictions <- function(df, fit, tback=10, q=c(0.025, 0.975), tspan=NULL, f
     
     gg <- df |> 
         bind_cols(pred_summ) |> 
-        filter(if(is.null(tspan)) t >= tpred - tback else date %within% tspan) |> 
+        filter(if(is.null(tspan)) t >= (tpred - tback) else date %within% tspan) |> 
         ggplot(aes(date, count)) +
         geom_ribbon(aes(ymin=ymin, ymax=ymax), col="gray70", alpha=0.6) +
-        geom_point(col="steelblue", size=1.05, shape=1) +
+        geom_point(col="steelblue", size=1.05) +
         geom_line(aes(y=mean), col="tomato3") +
-        facet_wrap(~state, scales="free_y", nrow=3) +
+        facet_wrap(~state, scales="free_y", nrow=4) +
         scale_x_date(date_breaks="4 weeks", date_labels="%b %Y", guide=guide_axis(angle=45)) +
         labs(x="Weeks", y="Hospitalizations") +
         theme_bw() +
         theme(panel.spacing=unit(0, "mm"), axis.title=element_text(size=14))
     
     if (!is.null(file))
-        ggsave(file, gg, width=15, height=5)
+        ggsave(file, gg, width=14, height=8)
     gg
 }
 
-
+# assumes the most recent week of data is in first row, for current epiweek
 plot_seasonal <- function(df, fit) {
     ep_wk_fx <- fit$summary.random$epiweek |> 
         as_tibble()
@@ -81,7 +96,7 @@ plot_seasonal <- function(df, fit) {
     ggplot(ep_wk_fx, aes(ID, mean)) +
         geom_ribbon(aes(ymin=`0.025quant`, ymax=`0.975quant`), col="gray70", alpha=0.6) +
         geom_line(col="tomato3") +
-        geom_vline(xintercept=last(df$epiweek), col="steelblue", linetype="dashed") +
+        geom_vline(xintercept=first(df$epiweek), col="steelblue", linetype="dashed") +
         # facet_wrap(~state) +
         labs(x="Week of the year", y="Seasonal effect (log scale)") +
         theme_bw() +
